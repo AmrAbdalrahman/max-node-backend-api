a0e16d1e9fe7e96323bc24b0f0c512dd262b6754
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-03-01 18:33:24 +0200

    create user model

---
+++ app.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+
+const MONGODB_URI = 'mongodb://localhost:27017/posts';
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true}).then((db) => {
+    console.log('MONGO connectes');
+
+}).catch(error => console.log(error));
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString().replace(/:/g, '-')
+            + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+    multer({storage: fileStorage, fileFilter: fileFilter}).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+
+app.use(((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode;
+    const message = error.message;
+    res.status(status).json({message: message});
+}));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
